ee06d6fd98c0913f6ead4b5db501c38004929b14
Talha Kayani <talha.kayani@invozone.com>
2021-12-05 03:06:20 +0500

    adding cors

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { sequelize } = require("./models");
 const routes = require("./routes");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(cors());
 app.use("/", routes);
 
 app.listen(PORT, async () => {
