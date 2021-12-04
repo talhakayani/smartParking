dcd0746cac411f2ebccc860cf734bdf0060ed9eb
Talha Kayani <talha.kayani@invozone.com>
2021-12-04 21:35:48 +0500

    set status

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,10 +1,10 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 exports.getAllUsers = async (req, res, _next) => {
   try {
     const users = await User.findAll();
-    if (!users) throw new Error('No user found');
-    return res.json({ status: 200, message: 'user found', users });
+    if (!users) throw new Error("No user found");
+    return res.json({ status: 200, message: "user found", users });
   } catch (err) {
     return res.json({ status: 400, message: err.message });
   }
@@ -14,7 +14,7 @@ exports.login = async (req, res, _next) => {
   try {
     const { username, password } = req.body;
     if (!username || !password)
-      throw new Error('Please provide the username and password');
+      throw new Error("Please provide the username and password");
     const user = await User.findOne({
       where: {
         username: username,
@@ -22,20 +22,20 @@ exports.login = async (req, res, _next) => {
       },
     });
     if (!user)
-      return res.status(300).json({
-        status: 300,
-        message: 'Username/password incorrect',
+      return res.status(200).json({
+        status: 200,
+        message: "Username/password incorrect",
       });
-    if (user.status === 'pending')
-      return res.status(300).json({
-        status: 300,
+    if (user.status === "pending")
+      return res.status(200).json({
+        status: 200,
         message:
           "We're reviewing your account we will notify you once your acccount is approved!",
       });
     user.password = null;
     return res.status(200).json({
       status: 200,
-      message: 'login successfully',
+      message: "login successfully",
       user: user,
     });
   } catch (err) {
@@ -67,12 +67,12 @@ exports.signup = async (req, res, _next) => {
       !status ||
       !mobile_number
     )
-      throw new Error('please attach the body with proper details');
+      throw new Error("please attach the body with proper details");
     const user_signup = await User.create(req.body);
-    if (!user_signup) throw new Error('Unable to create user');
+    if (!user_signup) throw new Error("Unable to create user");
     return res.status(200).json({
       status: 200,
-      message: 'user created',
+      message: "user created",
       user: user_signup,
     });
   } catch (err) {
@@ -84,11 +84,11 @@ exports.getAllPendingAccounts = async (req, res, _next) => {
   try {
     const pendingAccounts = await User.findAll({
       where: {
-        status: 'pending',
+        status: "pending",
       },
     });
-    let message = 'Accounts found';
-    if (!pendingAccounts.length) message = 'No pending account found';
+    let message = "Accounts found";
+    if (!pendingAccounts.length) message = "No pending account found";
     return res.status(200).json({
       status: 200,
       message,
@@ -102,10 +102,10 @@ exports.getAllPendingAccounts = async (req, res, _next) => {
 exports.approveAccount = async (req, res, _next) => {
   try {
     const { id, status } = req.body;
-    if (!id || !status) throw new Error('Please attach the body');
+    if (!id || !status) throw new Error("Please attach the body");
     const approvedAccount = await User.update(
       {
-        status: 'approved',
+        status: "approved",
       },
       {
         where: {
@@ -113,8 +113,8 @@ exports.approveAccount = async (req, res, _next) => {
         },
       }
     );
-    let message = 'Account approved';
-    if (!approvedAccount) message = 'No account found';
+    let message = "Account approved";
+    if (!approvedAccount) message = "No account found";
     return res.status(200).json({ status: 200, message, approvedAccount });
   } catch (err) {
     return res.status(400).json({ status: 400, message: err.message });
@@ -125,11 +125,11 @@ exports.getApprovedAccounts = async (req, res, _next) => {
   try {
     const approvedAccounts = await User.findAll({
       where: {
-        status: 'approved',
+        status: "approved",
       },
     });
-    let message = 'Approved accounts reterived';
-    if (!approvedAccounts.length) message = 'No approved Accounts reterived';
+    let message = "Approved accounts reterived";
+    if (!approvedAccounts.length) message = "No approved Accounts reterived";
 
     return res.status(200).json({ status: 200, message, approvedAccounts });
   } catch (err) {
